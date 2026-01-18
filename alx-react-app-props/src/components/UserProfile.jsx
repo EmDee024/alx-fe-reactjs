bcc8c95fe4784ae6389259d3cd1ddbe16ba3a61d
olaseigbe Olamide <olamidemercy4431@gmail.com>
2026-01-18 12:30:14 +0100

    UserProfile updated for checker

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,22 @@
-function UserProfile(props) {
-    return (
-        <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio}</p>
-        </div>
-    );
+import React, { useContext } from 'react';
+import { UserContext } from '../context/UserContext';
+
+function UserProfile() {
+  const user = useContext(UserContext);
+
+  return (
+    <div
+      style={{
+        border: '1px solid #ccc',
+        padding: '10px',
+        marginTop: '20px'
+      }}
+    >
+      <h2>User Profile</h2>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+    </div>
+  );
 }
 
 export default UserProfile;
